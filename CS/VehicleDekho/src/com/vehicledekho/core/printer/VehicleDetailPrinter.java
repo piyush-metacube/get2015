393e6f716b44cfec70302ba8cdb7b02a66da1cf7
piyush-metacube <piyush.sharma@metacube.com>
2015-08-23 20:03:18 +0530

    Case Study one

---
+++ CS/VehicleDekho/src/com/vehicledekho/core/printer/VehicleDetailPrinter.java
@@ -0,0 +1,9 @@
+package com.vehicledekho.core.printer;
+
+import com.vehicledekho.core.Vehicle;
+
+public class VehicleDetailPrinter {
+	public static String printVehicleSpecification(Vehicle vehicle) {
+		return vehicle.toString();
+	}
+}
