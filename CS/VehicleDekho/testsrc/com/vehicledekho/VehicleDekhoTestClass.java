1f2b8535b5859ffff9526d5c446557ae48b49230
piyush-metacube <piyush.sharma@metacube.com>
2015-08-24 10:18:31 +0530

    Update VehicleDekhoTestClass.java

--- CS/VehicleDekho/testsrc/com/vehicledekho/VehicleDekhoTestClass.java
+++ CS/VehicleDekho/testsrc/com/vehicledekho/VehicleDekhoTestClass.java
@@ -9,7 +9,7 @@ import com.vehicledekho.core.Bike;
 import com.vehicledekho.core.Car;
 import com.vehicledekho.core.Vehicle;
 import com.vehicledekho.core.helper.VehicleHelper;
-
+import com.vehicledekho.core.printer;
 public class VehicleDekhoTestClass {
 
 	@Test
@@ -24,17 +24,19 @@ public class VehicleDekhoTestClass {
 		assertEquals(true, vehicle instanceof Bike);
 	}
 
-	@Test
-	public void VehicleDetailPrinterTestForCar() {
-		Vehicle vehicle=VehicleHelper.createVehicle("car", "Sedan", "2008");
-		String expString="Vehicle [make=2008, model=Sedan, engineInCC=1225, fuelCapacity=400, mileage=17.25, price=650000.0, roadTax=5400.0]Car [isAcAvailable=true, powerSteering=true, accessoryKit=true]";
-		assertEquals(expString,vehicle.toString());
-	}
-	
-	@Test
-	public void VehicleDetailPrinterTestForBike() {
-		Vehicle vehicle=VehicleHelper.createVehicle("bike", "CafeRacer", "2008");
-		String expString="Vehicle [make=2008, model=CafeRacer, engineInCC=125, fuelCapacity=40, mileage=60.0, price=80000.0, roadTax=2400.0]Bike [selfStart=true, helmetPrice=500.8]";
-		assertEquals(expString,vehicle.toString());
-	}
+	 @Test
+    	public void VehicleDetailPrinterTestForCar() {
+		Vehicle vehicle = VehicleHelper.createVehicle("car", "Sedan", "2008");
+		String outputString = VehicleDetailPrinter.printVehicleSpecification(vehicle);
+		String expString = "Vehicle [make=2008, model=Sedan, engineInCC=1225, fuelCapacity=400, mileage=17.25, price=650000.0, roadTax=5400.0]Car [isAcAvailable=true, powerSteering=true, accessoryKit=true]";
+		assertEquals(expString, outputString);
+    	}
+
+    	@Test
+    	public void VehicleDetailPrinterTestForBike() {
+		Vehicle vehicle = VehicleHelper.createVehicle("bike", "CafeRacer", "2008");
+		String outputString = VehicleDetailPrinter.printVehicleSpecification(vehicle);
+		String expString = "Vehicle [make=2008, model=CafeRacer, engineInCC=125, fuelCapacity=40, mileage=60.0, price=80000.0, roadTax=2400.0]Bike [selfStart=true, helmetPrice=500.8]";
+		assertEquals(expString, outputString);
+    	}
 }
