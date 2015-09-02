62957c66f4e87d70e39e22e982c6160c06df069f
piyush-metacube <piyush.sharma@metacube.com>
2015-09-02 17:02:56 +0530

    Update BinarySearch.java

--- DS/dsassignment7/BinarySearch.java
+++ DS/dsassignment7/BinarySearch.java
@@ -10,7 +10,7 @@ public class BinarySearch {
 	    return index;
     }
 
-    // binary search recursive method
+    // binary search loop invariant
     public int recursiveBinarySearch(int[] sortedArray, int start, int end, int key) {
 
 	if (start < end) {
@@ -30,23 +30,4 @@ public class BinarySearch {
 	}
 	return -1; // if value is not found
     }
-
-    // binary search method loop variant
-    public int binarySearchWithLoop(int[] array, int value) {
-	int start = 0;
-	int end = array.length - 1;
-
-	for (int i = 0; i < array.length; i++) {
-	    int mid = (end + start) / 2;
-
-	    if (array[i] == value)
-		return i;
-	    else if (array[mid] > value)
-		end = mid - 1;
-	    else
-		start = mid + 1;
-	}
-	return -1; // if value is not found
-    }
-
 }
